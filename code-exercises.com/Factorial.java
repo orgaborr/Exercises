88c7b1a0b8b04de0c6fe23fce33d01e93cc8d46f
orgaborr <45071962+orgaborr@users.noreply.github.com>
2019-04-10 17:22:58 +0200

    Factorial.java filled

--- code-exercises.com/Factorial.java
+++ code-exercises.com/Factorial.java
@@ -1,1 +1,14 @@
+/*Write a method that calculates the factorial of a given number.
+Factorial is the product of all positive integers less than or equal to n. For example, factorial(4) = 4x3x2x1 = 24.
+TIP: To make it more interesting, try to do it recursively.*/
 
+public Integer factorial(Integer n) {
+  if(n <= 0) {
+    return -1; //error
+  }
+  int multi = 1;
+  for(int i=1; i<=n; i++) {
+    multi *= i;
+  }
+  return multi;
+}
